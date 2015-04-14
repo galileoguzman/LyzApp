a8f2c43cbfd6f28cf90b16a78728af0b699b1cf1
Galileo Guzmán <galileoguzman@gmail.com>
2015-04-13 23:39:17 -0500

    Initial Commit

---
+++ LyzApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LyzApp
+//
+//  Created by Galileo Guzman on 13/04/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LyzApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LyzApp
+//
+//  Created by Galileo Guzman on 13/04/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
